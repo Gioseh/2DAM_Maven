4d1f96b770ff3ea227f4857439e8d334d6566837
Sergio <sergiorosagarcia07@gmail.com>
2024-10-18 13:51:06 +0200

    10/10 Clase

--- src/main/java/es/cheste/AD/UD2_DataBaseConnectors/TablaFcturas/TablaFacturas.java
+++ src/main/java/es/cheste/AD/UD2_DataBaseConnectors/TablaFcturas/TablaFacturas.java
@@ -4,9 +4,7 @@ import es.cheste.AD.UD2_DataBaseConnectors.EjemploPreparedStmt.ConexionBD;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class TablaFacturas {
     private static final Logger LOGGER = LogManager.getRootLogger();
@@ -16,7 +14,9 @@ public class TablaFacturas {
         ConexionBD conexion = new ConexionBD();
         Connection con = conexion.getConnection();
         //crearTablaFacturas(con);
-        crearTablaLineaFacturas(con);
+        //crearTablaLineaFacturas(con);
+        //insertarLineasFacturas(con);
+        delimiter(con);
         conexion.desconectar();
     }
 
@@ -54,6 +54,80 @@ public class TablaFacturas {
         } catch (SQLException e) {
             LOGGER.error("Error al crear la tabla clientes1", e);
         }
+    }
+
+    private static void insertarLineasFacturas(Connection conexion) {
+        try (PreparedStatement sInsertFact = conexion.prepareStatement("INSERT INTO FACTURAS(DNI_CLIENTE) VALUES (?)",PreparedStatement.RETURN_GENERATED_KEYS);
+        PreparedStatement sInsertLineasFact = conexion.prepareStatement("INSERT INTO LINEAS_FACTURAS(NUM_FACTURA, LINEA_FACTURA, CONCEPTO, CANTIDAD) VALUES (?,?,?,?);");){
+            conexion.setAutoCommit(false);
+
+            int i =1;
+            sInsertFact.setString(i++,"78901234x");
+            sInsertFact.executeUpdate();
+            ResultSet rs = sInsertFact.getGeneratedKeys();
+            rs.next();
+            int numFact = rs.getInt(1);
+
+            int lineaFact = 1;
+            i = 1;
+            sInsertLineasFact.setInt(i++,numFact);
+            sInsertLineasFact.setInt(i++,lineaFact++);
+            sInsertLineasFact.setString(i++,"TUERCAS");
+            sInsertLineasFact.setInt(i++,25);
+            sInsertLineasFact.executeUpdate();
+
+            i = 1;
+            sInsertLineasFact.setInt(i++,numFact);
+            sInsertLineasFact.setInt(i++,lineaFact++);
+            sInsertLineasFact.setString(i++,"TORNILLOS");
+            sInsertLineasFact.setInt(i++,250);
+            sInsertLineasFact.executeUpdate();
+
+            conexion.commit();
+        } catch (SQLException e) {
+            muestraErrorSQL(e);
+            try{
+                conexion.rollback();
+                System.err.println("Se hace ROLLBACK");
+            } catch (Exception ex) {
+                System.err.println("Error haciendo ROLLBACK");
+                ex.printStackTrace(System.err);
+            }
+
+        }
+    }
+
+    private static void delimiter(Connection conexion) {
+        try(CallableStatement callableStatement = conexion.prepareCall("{call listado_parcial_clientes(?,?)}")) {
+            callableStatement.setString(1,"78901234x");
+            callableStatement.setInt(2,0);
+            callableStatement.registerOutParameter(2,java.sql.Types.VARCHAR);
+
+            callableStatement.execute();
+
+            ResultSet rs = callableStatement.getResultSet();
+
+            int inout_long = callableStatement.getInt(2);
+            System.out.println("-> inout_long: "+inout_long);
+            int nCli = 0;
+
+            while(rs.next()) {
+                System.out.println("[" + (++nCli) + "]: ");
+                System.out.println("DNI: "+rs.getString("DNI"));
+                System.out.println("APELLIDOS: "+rs.getString("APELLIDOS"));
+            }
+
+        } catch (SQLException e) {
+            muestraErrorSQL(e);
+        } catch (Exception e){
+            e.printStackTrace(System.err);
+        }
+    }
+
+    public static void muestraErrorSQL(SQLException e){
+        System.err.println("SQL ERROR mensaje: "+e.getMessage());
+        System.err.println("SQL Estado: "+e.getSQLState());
+        System.err.println("SQL código especifico: "+e.getErrorCode());
     }
 }
 
